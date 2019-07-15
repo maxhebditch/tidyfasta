026897b9fc927cc59c3373985a6e005b2768346d
Max Hebditch <max@maxhebditch.co.uk>
2019-07-15 15:33:47 +0100

    better handling of blank space and errors

--- tidy_fasta.py
+++ tidy_fasta.py
@@ -4,33 +4,32 @@
 import re
 import argparse
 from shutil import copy2
+import sys
+
+# def excepthook(type, value, traceback):
+#     print(value)
+
+# sys.excepthook =  excepthook
 
 def read_fasta(inputfile):
 
     read_file_array = []
-    badAA = False
 
     with open (inputfile, "r") as fasta:
         for idx, line in enumerate(fasta):
-            if not line.startswith(">") and re.match("^[bjouxBJOUX]+.*", line):
-                print(f"Unrecognised amino acids at line {idx}")
-                print(re.match("^[bjouxBJOUX]+.*", line)[0])
-                badAA = True
-            else:
-                read_file_array.append(line.replace("\n",""))
-
-    if badAA:
-        raise ValueError("Non-standard AA included")
+            read_file_array.append(line.replace("\n",""))
 
     return read_file_array
-
 
 def remove_blanks(original_array):
 
     tidy_array = []
 
     for idx, line in enumerate(original_array):
-        if line == "":
+        if idx == 0:
+            if line != "":
+                tidy_array.append(line)
+        elif line == "":
             if original_array[idx-1] != "":
                 tidy_array.append(line)
         else:
@@ -46,12 +45,12 @@ def name_lines(unnamed_array):
 
     for idx, line in enumerate(unnamed_array):
         if line.startswith(">"):
-            if idx != len(unnamed_array):
+            if idx != len(unnamed_array)-1:
                 if re.match("^[a-zA-Z]+.*", unnamed_array[idx+1]):
                     named_array.append(line)
                 else:
                     lonely_ID = True
-                    print(line)
+                    print(f"ID {line} has no sequence identified")
         elif idx == 0:
             if re.match("^[a-zA-Z]+.*", line):
                 named_array.append(f">protein-sol-{id_num}")
@@ -62,54 +61,64 @@ def name_lines(unnamed_array):
                 if line == "" and not unnamed_array[idx+1].startswith(">"):
                     named_array.append(f">protein-sol-{id_num}")
                     id_num += 1
-                else:
-                    named_array.append(line)
             if idx != len(unnamed_array):
                 if re.match("^[a-zA-Z]+.*", line):
                     named_array.append(line)
 
     if lonely_ID:
-        raise ValueError("IDs without sequence identified")
+        raise ValueError(f"IDs with no sequence identified: stopping")
                 
     return named_array
 
 def combine_lines(uncombined_array):
 
-    def testseq(seq,idx):
-    
+    def testseq(seq,name):
+        global badAA
+
         strseq = "".join(seq)
-        if re.match("^[a-z]+.*", strseq):
-            print(f"Lowercase aa code found on line {idx}, converting to uppercase")
+
+        if re.search("[a-z]", strseq):
+            print(f"Lowercase AA code found for sequence {name}, converting to uppercase")
             strseq = strseq.upper()
+
+        if re.search("[bjouxBJOUX]", strseq):
+            print(f"Unrecognised AA code found for sequence {name}")
+            badAA = True
 
         return strseq
 
     combined_array = []
     combiner = []
+    name = None
 
     for idx, line in enumerate(uncombined_array):
-        if idx == 0:
+        if line.startswith(">"):
+            newseq = testseq(combiner,name)
+            combined_array.append(newseq)
             combined_array.append(line)
-        else:
-            if line.startswith(">"):
-                newseq = testseq(combiner,idx)
-                combined_array.append(newseq)
-                combined_array.append("")
-                combined_array.append(line)
-                combiner = []
-            elif re.match("^[a-zA-Z]+.*", line):
-                combiner.append(line)
+            combiner = []
+            name = line
+        elif re.search("^[a-zA-Z]", line):
+            combiner.append(line)
 
-    newseq = testseq(combiner,idx)
+    newseq = testseq(combiner,name)
     combined_array.append(newseq)
     return combined_array
 
 def write_fasta(clean_array,outputfile):
+    start = False
     with open(outputfile, "w") as output:
-        for line in clean_array:
-            output.write(line+"\n")
+        for idx,line in enumerate(clean_array):
+            if line.startswith(">"):
+                start = True
+            if start:
+                output.write(line+"\n")
+                if idx % 2 != 1:
+                    if idx != len(clean_array)-1:
+                        output.write("\n")
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, help="Input file name")
     args = parser.parse_args()
@@ -117,11 +126,17 @@ if __name__ == "__main__":
     inputfile  = args.input
     outputfile = str(inputfile+"-formatted")
 
+    badAA = False
+
     fasta_array = read_fasta(inputfile)
     tidy_array = remove_blanks(fasta_array)
     named_array = name_lines(tidy_array)
     final_array = combine_lines(named_array)
-    write_fasta(final_array,outputfile)
 
-    copy2(inputfile,str(inputfile+"-old"))
-    copy2(outputfile,inputfile)
+    if badAA:
+        raise ValueError("Non-standard AA identified: stopping")
+    else:
+        write_fasta(final_array,outputfile)
+
+        copy2(inputfile,str(inputfile+"-old"))
+        copy2(outputfile,inputfile)
