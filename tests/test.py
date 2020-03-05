91566e390d37708085cb078dc9160af3a34061ab
Max Hebditch <max@maxhebditch.co.uk>
2020-03-05 13:23:01 +0000

    setting up and deleting files for tests now handled by functions

--- tests/test.py
+++ tests/test.py
@@ -15,30 +15,42 @@ def module_from_file(module_name, file_path):
 
 tidy_fasta = module_from_file("tidy_fasta", "../tidy_fasta/tidy_fasta.py")
 
-class TestSum(unittest.TestCase):
+def set_up_files(testfile_name):
+        original_name     = "./inputs/" + testfile_name + ".txt"
+        intermediate_name = "./inputs/" + testfile_name + ".txt-old"
+        reference_name    = "./outputs/" + testfile_name + ".txt"
+        
+        return original_name, intermediate_name, reference_name
 
-    def test_missing_name(self):
-        original_name     = "./inputs/test_missing_name.txt"
-        intermediate_name = "./inputs/test_missing_name.txt-old"
-        output_name       = "./inputs/test_missing_name.txt_output"
-        reference_name    = "./outputs/test_missing_name.txt"
+def tidy_up_files(original_name,intermediate_name):
+        os.remove(original_name)
+        os.rename(intermediate_name,original_name)
 
-        tidy_fasta.tidy_fasta(original_name,False)
+def assert_test_match(self,testfile_name,single):
+        original_name, intermediate_name, reference_name = set_up_files(testfile_name)
+
+        tidy_fasta.tidy_fasta(original_name,single)
 
         self.assertTrue(filecmp.cmp(original_name, reference_name, shallow=False))
 
-        os.remove(original_name)
-        os.rename(intermediate_name,original_name)
+        tidy_up_files(original_name,intermediate_name)
+
+
+class TestSum(unittest.TestCase):
+
+    def test_gold_standard(self):
+
+        assert_test_match(self,"test_gold_standard",False)
+
+    def test_missing_name(self):
+
+        assert_test_match(self,"test_missing_name",False)
 
     def test_bad_chars_sequence(self):
         original_name     = "./inputs/test_bad_chars_sequence.txt"
 
         with self.assertRaises(ValueError):
             tidy_fasta.tidy_fasta(original_name,False)
-        #self.assertRaises(ValueError,tidy_fasta.tidy_fasta(original_name,False))
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
