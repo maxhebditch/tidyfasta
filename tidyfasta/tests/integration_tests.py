98197f232462ba74b3c0c87b813bf07dc73ecca5
Max Hebditch <max@maxhebditch.co.uk>
2020-03-09 15:35:42 +0000

    Test for handling and writing multiline fasta_sequences

--- tidyfasta/tests/integration_tests.py
+++ tidyfasta/tests/integration_tests.py
@@ -21,6 +21,25 @@ class IntegrationTests(unittest.TestCase):
         self.assertEqual("> alirocumab", valid_array[0].ID)
         self.assertEqual("MVKVYAPASSANMSVGFDVLGAAVTPVDGALLGDVVTVEAAETF", valid_array[0].sequence)
 
+    def test_class_method_ProcessFASTA_get_fasta_gold_standard(self):
+
+        input_file = get_test_dir() + "/inputs/test_gold_standard_multiple.txt"
+        output_file = get_outputfile(input_file)
+
+        new = ProcessFasta(input_file, False, False)
+        new.write_FASTA()
+
+        try:
+            renamed_file = get_test_dir() + "/outputs/test_gold_standard_multiple.txt"
+            os.rename(output_file, renamed_file)
+
+            standard_file = get_test_dir() + "/standards/test_gold_standard_multiple.txt"
+
+            self.assertTrue(filecmp.cmp(renamed_file, standard_file, shallow=False))
+        finally:
+            os.remove(renamed_file)
+
+
     def test_class_method_ProcessFASTA_get_fasta_excess_whitespace_multi(self):
 
         test_ProcessFasta = ProcessFasta(get_test_dir() + "/inputs/test_excess_whitespace_multi.txt", False, False)
