03a8dea44fb28b4eb9abb1285240504520ba99a5
Max Hebditch <max@maxhebditch.co.uk>
2020-03-10 10:54:18 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tidyfasta',
+    version='1',
+    packages=['tidyfasta', 'tidyfasta.common'],
+    url='https://github.com/maxhebditch/tidyfasta',
+    license='MIT',
+    author='Max Hebditch',
+    author_email='max@maxhebditch.co.uk',
+    description='Sanitise protein FASTA files / data'
+)
